c63b24cfb5159b98d810503658eafb89e521a73b
Muhammad Tayyab Khalid <85389439+Tkhalid993@users.noreply.github.com>
2023-04-03 21:44:06 +0500

    Create Question no. 3.js

---
+++ Question no. 3.js
@@ -0,0 +1,17 @@
+
+/* 3. Write a program that takes an array of
+integers and prints the sum of all the elements 
+the array. */
+
+
+
+let arr = [1,2,3,4,5];
+
+let sum = 0;
+
+for (i = 0; i<arr.length; i++){
+    sum += arr[i];
+}
+
+console.log(sum);
+
