5f39de869c8fcdd0959d5264ab842c097d019108
Muhammad Tayyab Khalid <85389439+Tkhalid993@users.noreply.github.com>
2023-04-03 21:42:38 +0500

    Create Question no. 1.js

---
+++ Question no. 1.js
@@ -0,0 +1,10 @@
+
+/* 1. Declare an array and print each and every 
+element to the console.*/
+
+
+let arr = ["Pakistan",70,true,false];
+
+for (i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
