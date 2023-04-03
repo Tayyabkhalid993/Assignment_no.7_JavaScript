e79560d04edad8088139b170c47611513a9ad3da
Muhammad Tayyab Khalid <85389439+Tkhalid993@users.noreply.github.com>
2023-04-03 21:43:34 +0500

    Create Question no. 2.js

---
+++ Question no. 2.js
@@ -0,0 +1,16 @@
+
+/* 2. Write a function which takes an array as
+argument and returns all array elements.
+*/
+
+
+let arr = [23,78,89];
+
+function fun (x){
+    return x ;
+}
+
+let a;
+a = fun (arr);
+console.log(a);
+
